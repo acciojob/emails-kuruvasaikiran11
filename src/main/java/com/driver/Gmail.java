b18e873c4bcca4b29bd304972de40097fdf32c26
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2022-12-30 01:17:25 +0530

    Commit3

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -27,9 +27,9 @@ public class Gmail extends Email {
         // 1. Each mail in the inbox is distinct.
         // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
         if(Inbox.size() == inboxCapacity){
-            Triple<Date, String, String> oldMail = Inbox.get(0);
+            Triple<Date, String, String> oldestMail = Inbox.get(0);
             Inbox.remove(0);
-            Trash.add(oldMail);
+            Trash.add(oldestMail);
         }
         Triple<Date, String, String> mail = Triple.of(date, sender, message);
         Inbox.add(mail);
