da96f0c8ea885ff13e914fc1ce68388a2913591e
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2022-12-30 01:06:56 +0530

    Commit2

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -19,19 +19,22 @@ public class Email {
     }
 
     public void changePassword(String oldPassword, String newPassword){
-        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
-        // 1. It contains at least 8 characters
-        // 2. It contains at least one uppercase letter
-        // 3. It contains at least one lowercase letter
-        // 4. It contains at least one digit
-        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        /*
+        Change password only if the oldPassword is equal to current password and the new password meets
+         all the following:
+         1. It contains at least 8 characters
+         2. It contains at least one uppercase letter
+         3. It contains at least one lowercase letter
+         4. It contains at least one digit
+         5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        */
         if(oldPassword.equals(password)){
             if(isValid(newPassword)) {
-                System.out.println("Password changed succesfully!");
+                System.out.println("Password changed successfully!");
                 this.password = newPassword;
             }
             else
-                System.out.println("The new password is not invalid!");
+                System.out.println("The new password is not valid!");
         }
         else
             System.out.println("The given password does not match current password!");
@@ -55,8 +58,6 @@ public class Email {
                 digit = true;
             else specialChar = true;
         }
-        if(capitalLetter && smallLetter && digit && specialChar)
-            return true;
-        return false;
+        return capitalLetter && smallLetter && digit && specialChar;
     }
 }
